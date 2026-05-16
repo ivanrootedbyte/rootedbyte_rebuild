218ddd4bfba95a4fb7fa6398f6fa718920097ce8
ivanrootedbyte <ivanrootedbyte@gmail.com>
2026-05-16 15:21:09 -0700

    Update shared.js

--- api/shared.js
+++ api/shared.js
@@ -22,7 +22,7 @@ module.exports = async function handler(req, res) {
     });
   }
 
-  const supabaseUrl = process.env.SUPABASE_URL;
+  const supabaseUrl = String(process.env.SUPABASE_URL || "").replace(/\/+$/, "");
   const serviceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
   if (!supabaseUrl || !serviceRoleKey) {
